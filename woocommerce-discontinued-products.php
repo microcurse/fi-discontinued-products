2dd4da59ba4bd27588cccae50f254e457ccaa557
Marc <marc.maninang@gmail.com>
2023-04-10 11:08:27 -0700

    Merged 1.1.5

--- woocommerce-discontinued-products.php
+++ woocommerce-discontinued-products.php
@@ -2,11 +2,11 @@
 /**
  * Plugin Name: WooCommerce Discontinued Products
  * Plugin URI: https://wordpress.org/plugins/discontinued-products/
- * Description: Enables WooCommerce Discontinued Products.
- * Author: Leon @ 211J
+ * Description: Enables WooCommerce Discontinued Products. Modified for Forbes Industries
+ * Author: Marc M.
  * Author URI: http://211j.com/
- * Version: 1.1.7
- * Text Domain: discontinued-products
+ * Version: 1.1.6
+ * Text Domain: woocommerce-discontinued-products
  * Domain Path: /languages
 
  * License: GNU General Public License v3.0
@@ -20,20 +20,85 @@
  */
 if ( in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', get_option( 'active_plugins' ) ), true ) ) {
 
-	if ( ! defined( 'DP_URI' ) ) {
-		define( 'DP_URI', plugins_url( '', __FILE__ ) );
+	if ( ! class_exists( 'WC_Discontinued_Products' ) ) {
+
+		if ( ! defined( 'WC_DP_URI' ) ) {
+			define( 'WC_DP_URI', plugins_url( '', __FILE__ ) );
+		}
+
+		if ( ! defined( 'WC_DP_PATH' ) ) {
+			define( 'WC_DP_PATH', plugin_dir_path( __FILE__ ) );
+		}
+
+		/**
+		 * The main WooCommerce Discontinued Products Class.
+		 * Check if WooCommerce is active, load functions and other files.
+		 *
+		 * @since 1.0.0
+		 */
+		class WC_Discontinued_Products {
+
+			/**
+			 * Inititiate the WC_Discontinued_Products Class.
+			 *
+			 * @since 1.0.0
+			 */
+			public function __construct() {
+				// Called only after woocommerce has finished loading.
+				add_action( 'woocommerce_init', array( $this, 'woocommerce_loaded' ) );
+				// Called after all plugins have loaded.
+				add_action( 'plugins_loaded', array( $this, 'plugins_loaded' ) );
+
+				// Called just before the woocommerce template functions are included.
+				add_action( 'init', array( $this, 'include_template_functions' ), 20 );
+
+				// Indicates we are running the admin.
+				if ( is_admin() ) {
+					is_admin();
+				}
+
+				// Indicates we are being served over ssl.
+				if ( is_ssl() ) {
+					is_ssl();
+				}
+
+				// Take care of anything else that needs to be done immediately upon plugin instantiation, here in the constructor.
+			}
+
+			/**
+			 * Take care of anything that needs woocommerce to be loaded.
+			 * For instance, if you need access to the $woocommerce global
+			 */
+			public function woocommerce_loaded() {
+
+				include 'includes/wc-class-dp-csv-import-export.php';
+				include 'includes/wc-class-dp-discontinued-product.php';
+				include 'includes/wc-class-dp-settings.php';
+				include 'includes/wc-class-dp-shortcode-discontinued.php';
+			}
+
+			/**
+			 * Take care of anything that needs all plugins to be loaded
+			 */
+			public function plugins_loaded() {
+
+				/**
+				 * Localisation
+				 */
+				load_plugin_textdomain( 'woocommerce-discontinued-products', false, dirname( plugin_basename( __FILE__ ) ) . '/' );
+			}
+
+			/**
+			 * Override any of the template functions from woocommerce/woocommerce-template.php
+			 * with our own template functions file
+			 */
+			public function include_template_functions() {
+
+				include 'woocommerce-template.php';
+			}
+		}
+
+		// Finally instantiate our plugin class and add it to the set of globals.
+		$GLOBALS['wc_discontinued_products'] = new WC_Discontinued_Products();
 	}
-
-	if ( ! defined( 'DP_PATH' ) ) {
-		define( 'DP_PATH', plugin_dir_path( __FILE__ ) );
-	}
-
-	if ( ! defined( 'DP_VER' ) ) {
-		define( 'DP_VER', '1.1.7' );
-	}
-
-	include DP_PATH . 'includes/class-discontinued-products.php';
-
-	// Finally instantiate our plugin class and add it to the set of globals.
-	$GLOBALS['wc_discontinued_products'] = new Discontinued_Products();
 }
--- woocommerce-template.php
+++ woocommerce-template.php
@@ -18,9 +18,10 @@ if ( ! function_exists( 'dp_is_discontinued' ) ) {
 	function dp_is_discontinued( $product_id = null ) {
 
 		global $post;
-		if ( $post || null !== $product_id ) {
-			$product_id = null !== $product_id ? $product_id : $post->ID;
-			return has_term( (int) get_option( 'dp_discontinued_term' ), 'product_discontinued', $product_id );
+		if ( $post || $product_id !== null ) {
+			$product_id      = $product_id !== null ? $product_id : $post->ID;
+			$is_discontinued = get_post_meta( $product_id, '_is_discontinued', true );
+			return $is_discontinued === 'yes';
 		}
 		return false;
 	}
@@ -41,11 +42,11 @@ if ( ! function_exists( 'dp_alt_products' ) ) {
 		$alt_products = is_array( $alt_products ) ? $alt_products : array();
 		$notice       = dp_alt_products_notice( $post->ID, empty( $alt_products ) );
 		?>
-		<?php echo wp_kses( $notice, 'post' ); ?></h4>
+		<h4 class="discontinued-notice"><?php echo esc_html( $notice ); ?></h4>
 		<?php
 		foreach ( $alt_products as $alt_product ) {
 			?>
-			<a href="<?php echo esc_url( get_permalink( $alt_product ) ); ?>" class="button"><?php echo esc_html( get_the_title( $alt_product ) ); ?></a>
+			<a href="<?php echo esc_url( get_permalink( $alt_product ) ); ?>" class="button"><?php echo get_the_title( $alt_product ); ?></a>
 			<?php
 		}
 	}
@@ -65,11 +66,11 @@ if ( ! function_exists( 'dp_alt_products_notice' ) ) {
 
 		$prod_text_option = get_post_meta( $product_id, '_discontinued_product_text', true );
 		$prod_alt_option  = get_post_meta( $product_id, '_alt_product_text', true );
-		$text_option      = get_option( 'dp_discontinued_text' );
-		$alt_option       = get_option( 'dp_alt_text' );
-		$text             = dp_alt_products_text( $prod_text_option, $text_option, __( 'This product has been discontinued.', 'discontinued-products' ) );
-		$alt              = dp_alt_products_text( $prod_alt_option, $alt_option, __( 'You may be interested in:', 'discontinued-products' ) );
-		$notice           = $no_alt ? '<h4 class="discontinued-notice">' . esc_html( $text ) . '</H4>' : '<h4 class="discontinued-notice">' . esc_html( $text ) . '</H4><h4 class="discontinued-notice-alt">' . esc_html( $alt ) . '</H4>';
+		$text_option      = get_option( 'dc_discontinued_text' );
+		$alt_option       = get_option( 'dc_alt_text' );
+		$text             = dp_alt_products_text( $prod_text_option, $text_option, __( 'This product has been discontinued.', 'woocommerce-discontinued-products' ) );
+		$alt              = dp_alt_products_text( $prod_alt_option, $alt_option, __( 'You may be interested in:', 'woocommerce-discontinued-products' ) );
+		$notice           = $no_alt ? $text : $text . ' ' . $alt;
 		return $notice;
 	}
 }
@@ -100,7 +101,7 @@ if ( ! function_exists( 'is_dp_shop' ) ) {
 	 * @return bool
 	 */
 	function is_dp_shop() {
-		return ( is_page( (int) get_option( 'dp_shop_page_id' ) ) );
+		return ( is_page( (int) get_option( 'dc_shop_page_id' ) ) );
 	}
 }
 
@@ -112,58 +113,17 @@ if ( ! function_exists( 'discontinued_template_loop_price' ) ) {
 
 	/**
 	 * Discontinued_template_loop_price - replaces price with discontinued text.
-	 * Replaces price in admin with "discontinued".
 	 *
-	 * @param int    $price Product price.
-	 * @param object $product Product object.
 	 * @return null
 	 */
 	function discontinued_template_loop_price( $price, $product ) {
 		$product_id = $product->get_id();
-		if ( ! is_single() && dp_is_discontinued( $product_id ) ) {
-			if ( is_admin() ) {
-				return 'Discontinued';
-			}
+		if ( dp_is_discontinued( $product_id ) ) {
 			$prod_text_option = get_post_meta( $product_id, '_discontinued_product_text', true );
-			$text_option      = get_option( 'dp_discontinued_text' );
-			$text             = dp_alt_products_text( $prod_text_option, $text_option, __( 'This product has been discontinued.', 'discontinued-products' ) );
-			$price            = $price . '<br>' . $text;
+			$text_option      = get_option( 'dc_discontinued_text' );
+			$text             = dp_alt_products_text( $prod_text_option, $text_option, __( 'This product has been discontinued.', 'woocommerce-discontinued-products' ) );
+			$price            = $text;
 		}
 		return $price;
-	}
-}
-add_filter( 'woocommerce_is_purchasable', 'discontinued_is_purchasable', 100, 2 );
-
-if ( ! function_exists( 'discontinued_is_purchasable' ) ) {
-
-	/**
-	 * Set is_purchasable to false if product is discontinued.
-	 *
-	 * @param int    $is_purchasable Product is_purchasable.
-	 * @param object $product Product object.
-	 * @return null
-	 */
-	function discontinued_is_purchasable( $is_purchasable, $product ) {
-		if ( dp_is_discontinued( $product->get_id() ) ) {
-			$is_purchasable = false;
-		}
-		return $is_purchasable;
-	}
-}
-
-add_filter( 'woocommerce_product_price_class', 'discontinued_template_price_class', 10, 1 );
-
-if ( ! function_exists( 'discontinued_template_price_class' ) ) {
-
-	/**
-	 * Discontinued_template_price_class - Add "discontinued" to the price class.
-	 *
-	 * @param int $class Product price css class.
-	 * @return null
-	 */
-	function discontinued_template_price_class( $class ) {
-		$class = $class . ' discontinued';
-
-		return $class;
 	}
 }
